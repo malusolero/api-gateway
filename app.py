119144127e32612c1a4cf911e87732bf12c18dc5
Maria Lucia Solero <marialucia.solero@gmail.com>
2023-09-29 16:06:54 +0100

    More routes

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from schemas import UserServiceCreateBodySchema, HeaderSchema
+from schemas import UserServiceCreateBodySchema, HeaderSchema, ProductServiceSearchSchema, ProductServiceCreateOrderSchema, ProductServiceProductPathSchema, ProductServiceCreateProductSchema
 from os import environ
 from flask_cors import CORS
 from flask_openapi3 import OpenAPI, Info, Tag
-from flask import redirect
+from flask import redirect, jsonify
 from sqlalchemy.exc import IntegrityError
 import requests
 from dotenv import load_dotenv
@@ -22,6 +22,13 @@ api_gateway = Tag(name="API Gateway",
 
 
 LOGIN_SERVICE_URL = environ.get('LOGIN_SERVICE')
+PRODUCT_SERVICE_URL = environ.get('PRODUCT_SERVICE')
+
+
+def transform_authorization_header(header: HeaderSchema):
+    return {
+        "Authorization": header.authorization
+    }
 
 
 @app.get('/', tags=[home_tag])
@@ -30,7 +37,7 @@ def home():
     """
     return redirect('/openapi')
 
-# LOGIN SERVICE REDIRECTS
+# LOGIN SERVICE redirect
 
 
 @app.post('/user', tags=[api_gateway])
@@ -38,17 +45,15 @@ def redirect_create_user(body: UserServiceCreateBodySchema):
     '''
         Redirects to user service create user route, check user service swagger for more details
     '''
+    json = {"username": body.username, "password": body.password}
+    response = requests.post(
+        f'{LOGIN_SERVICE_URL}/user', json=json)
     try:
-        json = {"username": body.username, "password": body.password}
-        print(json)
-        response = requests.post(
-            f'{LOGIN_SERVICE_URL}/user', json=json)
-        print(response.text)
         return response.text, response.status_code
     except Exception as e:
         print(e)
         error_message = f'{e}'
-        return {"message": error_message}, 400
+        return {"message": error_message}, response.status_code
 
 
 @app.post('/user/login', tags=[api_gateway])
@@ -56,17 +61,14 @@ def redirect_login(body: UserServiceCreateBodySchema):
     '''
         Redirects to user service login route, check user service swagger for more details
     '''
+    json = {"username": body.username, "password": body.password}
+    response = requests.post(
+        f'{LOGIN_SERVICE_URL}/user/login', json=json)
     try:
-        json = {"username": body.username, "password": body.password}
-        print(json)
-        response = requests.post(
-            f'{LOGIN_SERVICE_URL}/user/login', json=json)
-        print(response.text)
         return response.text, response.status_code
     except Exception as e:
-        print(e)
         error_message = f'{e}'
-        return {"message": error_message}, 400
+        return {"message": error_message}, response.status_code
 
 
 @app.get('/user/is-authenticated', tags=[api_gateway])
@@ -74,12 +76,180 @@ def redirect_user_is_authenticated(header: HeaderSchema):
     '''
         Redirects to user service is authenticated route, check user service swagger for more details
     '''
+    response = requests.get(
+        f'{LOGIN_SERVICE_URL}/user/is-authenticated', headers=transform_authorization_header(header))
     try:
-        print('HEADEEEER')
-        print(header.authorization)
-
-        return {"message": 'ok'}, 200
+        return response.text, response.status_code
     except Exception as e:
-        print(e)
         error_message = f'{e}'
-        return {"message": error_message}, 400
+        return {"message": error_message}, response.status_code
+
+
+@app.put('/user', tags=[api_gateway])
+def redirect_update_user(header: HeaderSchema, body: UserServiceCreateBodySchema):
+    """
+        Redirects to user service update user route, check user service swagger for more details
+    """
+    json = {"username": body.username, "password": body.password}
+    response = requests.put(
+        f'{LOGIN_SERVICE_URL}/user', headers=transform_authorization_header(header), json=json)
+
+    try:
+        return response.text, response.status_code
+    except Exception as e:
+        error_message = f'{e}'
+        return {"message": error_message}, response.status_code
+
+
+@app.delete('/user', tags=[api_gateway])
+def redirect_delete_user(header: HeaderSchema):
+    """
+        Redirects to user service update user route, check user service swagger for more details
+    """
+    response = requests.delete(
+        f'{LOGIN_SERVICE_URL}/user', headers=transform_authorization_header(header))
+    try:
+        return response.text, response.status_code
+    except Exception as e:
+        error_message = f'{e}'
+        return {"message": error_message}, response.status_code
+
+# PRODUCT SERVICE redirect
+
+# public routes
+
+
+@app.get('/product', tags=[api_gateway])
+def redirect_get_product():
+    """
+        Redirects to product service get product route, check user service swagger for more details
+    """
+    response = requests.get(
+        f'{PRODUCT_SERVICE_URL}/product',)
+    try:
+        return response.text, response.status_code
+    except Exception as e:
+        error_message = f'{e}'
+        return {"message": error_message}, response.status_code
+
+
+@app.get('/product/<int:product_id>', tags=[api_gateway])
+def redirect_get_product_id(path: ProductServiceProductPathSchema):
+    """
+        Redirects to product service get product by id route, check user service swagger for more details
+    """
+    response = requests.get(
+        f'{PRODUCT_SERVICE_URL}/product/{path.product_id}',)
+    try:
+        return response.text, response.status_code
+    except Exception as e:
+        error_message = f'{e}'
+        return {"message": error_message}, response.status_code
+
+
+@app.get('/product/search', tags=[api_gateway])
+def redirect_search_product(query: ProductServiceSearchSchema):
+    """
+        Redirects to product service get product route, check user service swagger for more details
+    """
+    if not query or not query.name:
+        return {"message": 'missing query parameter "name"'}, 400
+
+    response = requests.get(
+        f'{PRODUCT_SERVICE_URL}/product/search?name={query.name}',)
+    try:
+        return response.text, response.status_code
+    except Exception as e:
+        error_message = f'{e}'
+        return {"message": error_message}, response.status_code
+
+
+@app.post('/product/order', tags=[api_gateway])
+def redirect_order_product(body: ProductServiceCreateOrderSchema):
+    """
+        Redirects to product service create order route, check user service swagger for more details
+    """
+    json = {
+        "user_email": body.user_email,
+        "total_price": body.total_price,
+        "date": body.date,
+        "order_products": body.order_products
+    }
+    response = requests.post(
+        f'{PRODUCT_SERVICE_URL}/order', json=json)
+    try:
+        return response.text, response.status_code
+    except Exception as e:
+        error_message = f'{e}'
+        return {"message": error_message}, response.status_code
+
+
+# private routes
+@app.post('/product', tags=[api_gateway])
+def redirect_create_product(body: ProductServiceCreateProductSchema, header: HeaderSchema):
+    """
+         Redirects to product service create product route, check user service swagger for more details
+    """
+    logged_in = redirect_user_is_authenticated(header)
+    if logged_in['status_code'] != 200:
+        return {'message': 'Unauthorized'}, 401
+
+    json = {
+        "name": body.name,
+        "description": body.description,
+        "amount": body.amount,
+        "image": body.image,
+        "weight": body.weight,
+        "price": body.price,
+    }
+    response = requests.post(
+        f'{PRODUCT_SERVICE_URL}/product', json=json)
+    try:
+        return response.text, response.status_code
+    except Exception as e:
+        error_message = f'{e}'
+        return {"message": error_message}, response.status_code
+
+
+@app.put('/product/<int:product_id>', tags=[api_gateway])
+def redirect_update_product(body: ProductServiceCreateProductSchema, header: HeaderSchema, path: ProductServiceProductPathSchema):
+    """
+         Redirects to product service update product route, check user service swagger for more details
+    """
+    logged_in = redirect_user_is_authenticated(header)
+    if logged_in['status_code'] != 200:
+        return {'message': 'Unauthorized'}, 401
+
+    json = {
+        "name": body.name,
+        "description": body.description,
+        "amount": body.amount,
+        "image": body.image,
+        "weight": body.weight,
+        "price": body.price,
+    }
+    try:
+        response = requests.post(
+            f'{PRODUCT_SERVICE_URL}//product/{path.product_id}', json=json)
+        return response.text, response.status_code
+    except Exception as e:
+        error_message = f'{e}'
+        return {"message": error_message}, response.status_code
+
+
+@app.delete('/product/<int:product_id>', tags=[api_gateway])
+def redirect_delete_product(path: ProductServiceProductPathSchema, header: HeaderSchema):
+    """
+        Redirects to product service delete product by id route, check user service swagger for more details
+    """
+    logged_in = redirect_user_is_authenticated(header)
+    if logged_in['status_code'] != 200:
+        return {'message': 'Unauthorized'}, 401
+
+    response = requests.get(
+        f'{PRODUCT_SERVICE_URL}/product/{path.product_id}',)
+    try:
+        return response.text, response.status_code
+    except Exception as e:
+        error_message = f'{e}'
+        return {"message": error_message}, response.status_code
--- schemas/redirect.py
+++ schemas/redirect.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import List
 
 
 class UserServiceCreateBodySchema(BaseModel):
@@ -16,5 +16,45 @@ class HeaderSchema(BaseModel):
     """
         Header Schema used for receiving token inside Authorization Header
     """
-    authorization: Optional[str] = Field(
-        alias="Authorization", example='Authorization Bearer JWT_TOKEN')
+    authorization: str = Field(...,
+                               alias="Authorization", example='Authorization Bearer JWT_TOKEN')
+
+
+class ProductServiceSearchSchema(BaseModel):
+    """ Product will be searched by name
+    """
+    name: str = Field(example="Detergente",
+                      description="Name of the product that you want to search for")
+
+
+class ProductServiceCreateOrderSchema(BaseModel):
+    """
+        Parameters for successfully create an order
+    """
+    user_email: str = 'test@email.com'
+    total_price: float = 120.00
+    date: str = '2023-09-20T19:15:24.588Z'
+    order_products: List = [
+        {"amount": 3, "price": 10.99, "product_id": 1},
+        {"amount": 6, "price": 1.99, "product_id": 2},
+        {"amount": 7, "price": 15.99, "product_id": 3},
+    ]
+
+
+class ProductServiceProductPathSchema(BaseModel):
+    """ Defines the url path product id
+    """
+    product_id: int = Field(description="Product id")
+
+
+class ProductServiceCreateProductSchema(BaseModel):
+    """ Arguments needed for creating a product in the database
+    """
+
+    name: str = Field(example='Detergente para mãos')
+    description: str = Field(
+        example='Detergente potente para retirar todos os resíduos')
+    amount: int = Field(example=3)
+    weight: str = Field(example='1L')
+    image: str = Field(example='https://example.com')
+    price: float = Field(example=50.99)
